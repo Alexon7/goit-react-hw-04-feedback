e8dc4e58239fa2cc15a458c9c23300d8143e5667
Alexon7 <alenkaalexon@gmail.com>
2023-05-19 13:41:18 +0200

    change app on huks

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import Section from './components/Section';
+import FeedbackOptions from 'components/FeedbackOptions';
+import Statistics from './components/Statistics';
+import Notification from 'components/Notification';
+
+import React from 'react';
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const option = {
+    good,
+    neutral,
+    bad,
+  };
+
+  const onLeaveFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const total = good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / total) * 100);
+  };
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(option)}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={countPositiveFeedbackPercentage(total)}
+          />
+        )}
+      </Section>
+    </>
+  );
+};
